080172cc39e35c75644fc08c51e1e4a882bb3e5b
znergy <Guest@Epicodus-5A.local>
2017-03-27 15:51:08 -0700

    add REST routes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,5 +15,25 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/entries/:entryId", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Entry entry = Entry.find(Integer.parseInt(request.params(":entryId")));
+      model.put("entry", entry);
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/entries", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      String name = request.queryParams("name");
+      String phone = request.queryParams("phone");
+      String mail = request.queryParams("mail");
+      String email = request.queryParams("email");
+      Entry entry = new Entry(name, phone, mail, email);
+      model.put("entry", entry);
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
   }
 }
