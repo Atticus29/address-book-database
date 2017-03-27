75032846a775f030613e3141283074b290f1b47f
Mark Fisher and Ryan Jones <ryan@>
2017-03-27 15:51:37 -0700

    add skeleton of Entry class

--- src/main/java/Entry.java
+++ src/main/java/Entry.java
@@ -1,9 +1,32 @@
 import org.sql2o.*;
 
 public class Entry {
+  private String name;
+  private String phone;
+  private String address;
+  private String email;
 
-  public Entry() {
 
+  public Entry(String name, String phone, String address, String email) {
+    this.name = name;
+    this.phone = phone;
+    this.address = address;
+    this.email = email;
   }
+
+  public String getName(){
+    return name;
+  }
+
+  public String getPhone(){
+    return phone;
+  }
+  public String getAddress(){
+    return address;
+  }
+  public String getEmail(){
+    return email;
+  }
+
 
 }
