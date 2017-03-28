92b84b129e506093f23259c3a29aaea23bd058eb
Mark Fisher and Ryan Jones <ryan@>
2017-03-27 17:05:40 -0700

    fix tests, add equals method override, and create databases with id as first column to yield expected behavior

--- src/main/java/Entry.java
+++ src/main/java/Entry.java
@@ -5,6 +5,7 @@ public class Entry {
   private String phone;
   private String address;
   private String email;
+  private int id;
 
 
   public Entry(String name, String phone, String address, String email) {
@@ -28,5 +29,40 @@ public class Entry {
     return email;
   }
 
+  public int getId(){
+    return id;
+  }
 
+  @Override
+  public boolean equals(Object secondEntry){
+    if(!(secondEntry instanceof Entry)){
+      return false;
+    } else{
+      Entry testEntry = (Entry) secondEntry;
+      return (this.getName().equals(testEntry.getName()));
+    }
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO entries (name, phone, address, email) VALUES (:name, :phone, :address, :email);";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("name", this.name)
+        .addParameter("phone", this.phone)
+        .addParameter("address", this.address)
+        .addParameter("email", this.email)
+        .executeUpdate()
+        .getKey();
+    }
+  }
+
+  public static Entry find(int id){
+    try(Connection con = DB.sql2o.open()){
+      String sqlCommand = "SELECT * FROM entries WHERE id=:id;";
+      Entry foundEntry = con.createQuery(sqlCommand)
+        .addParameter("id", id)
+        .executeAndFetchFirst(Entry.class);
+      return foundEntry;
+    }
+  }
 }
--- src/test/java/EntryTest.java
+++ src/test/java/EntryTest.java
@@ -3,18 +3,39 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class EntryTest {
+  // private Entry testEntry;
 
   @Before
   public void setUp() {
-    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/address_book_test", null, null);
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/address_book_rjmf_test", null, null);
+
   }
 
   @After
   public void tearDown() {
     try (Connection con = DB.sql2o.open()) {
-      String sql = "DELETE FROM name_of_your_table *;";
+      String sql = "DELETE FROM entries *;";
       con.createQuery(sql).executeUpdate();
     }
+  }
+  //
+  @Test
+  public void find_returnsCorrectEntry_true(){
+    Entry testEntry = new Entry("Ryan", "555-555-5555", "123 Maple Lane Portland, OR 97203", "Ryan@isCool.com");
+    testEntry.save();
+    Entry testEntry2 = new Entry("Mark", "555-555-5556", "124 Maple Lane Portland, OR 97203", "Mark@isAlright.com");
+    testEntry2.save();
+    assertEquals(Entry.find(testEntry2.getId()), testEntry2);
+  }
+
+  @Test
+  public void save_SavesAnEntry_true(){
+    Entry testEntry = new Entry("Ryan", "555-555-5555", "123 Maple Lane Portland, OR 97203", "Ryan@isCool.com");
+    testEntry.save();
+    // Entry retrievedEntry = Entry.find(1);
+    Entry retrievedEntry = Entry.find(testEntry.getId());
+    // assertTrue(retrievedEntry instanceof Entry);
+    assertEquals("Ryan",retrievedEntry.getName());
   }
 
 }
